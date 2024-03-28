8016a7ce47810b63efe18ca886ce95f9e09454a0
btwise <38809560+btwise@users.noreply.github.com>
2024-03-28 22:10:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -156,3 +156,9 @@ sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/CONFIG_PACKAGE_luci-theme-bootst
 # sed -i 's/CONFIG_PACKAGE_kmod-mt7921-firmware=y/CONFIG_PACKAGE_kmod-mt7921-firmware=n/' .config
 # sed -i 's/CONFIG_PACKAGE_kmod-mt7921e=y/CONFIG_PACKAGE_kmod-mt7921e=n/' .config
 # sed -i 's/CONFIG_PACKAGE_kmod-mt7921u=y/CONFIG_PACKAGE_kmod-mt7921u=n/' .config
+patch -p0 < disable_flock.patch
+if [ $? -eq 0 ]; then
+    echo "Patch applied successfully"
+else
+    echo "Patch application failed"
+fi
