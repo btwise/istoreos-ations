6ee3e942e279edf12b04c2049ee075c7502c5ec5
btwise <38809560+btwise@users.noreply.github.com>
2024-03-31 16:03:02 +0800

    Update preset-clash-core.sh

--- preset-clash-core.sh
+++ preset-clash-core.sh
@@ -12,12 +12,14 @@ echo -e "预置Clash内核"
 mkdir -p feeds/openclash/luci-app-openclash/root/etc/openclash/core
 core_path="feeds/openclash/luci-app-openclash/root/etc/openclash/core"
 goe_path="feeds/openclash/luci-app-openclash/root/etc/openclash"
+all_run_path="package/istoreos-files/files/usr/sbin/"
 
 CLASH_DEV_URL="https://github.com/vernesong/OpenClash/raw/core/master/dev/clash-linux-amd64-v3.tar.gz"
 CLASH_TUN_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-2023.08.17-13-gdcc8d87.gz"
 CLASH_META_URL="https://github.com/vernesong/OpenClash/raw/core/master/meta/clash-linux-amd64-v3.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+ALLRUN_URL="https://raw.githubusercontent.com/wukongdaily/allinonescript/main/all/all.run"
 
 wget -qO- $CLASH_DEV_URL | tar xOvz > $core_path/clash
 wget -qO- $CLASH_TUN_URL | gunzip -c > $core_path/clash_tun
@@ -27,5 +29,7 @@ wget -qO- $CLASH_META_URL | tar xOvz > $core_path/clash_meta
 # wget $CLASH_META_URL && tar xOv $(basename $CLASH_META_URL) > $core_path/clash_meta && rm $(basename $CLASH_META_URL)
 wget -qO- $GEOIP_URL > $goe_path/GeoIP.dat
 wget -qO- $GEOSITE_URL > $goe_path/GeoSite.dat
+wget -P $all_run_path $ALLRUN_URL
 
 chmod +x $core_path/clash*
+chmod +x $all_run_path/all.run
